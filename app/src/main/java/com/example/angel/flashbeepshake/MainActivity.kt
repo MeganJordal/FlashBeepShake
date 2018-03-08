27c21f3cca8b8f584790e4706237b1d2899773ca
Megan Jordal <mjordal@msudenver.edu>
2018-03-08 13:02:49 -0700

    Working App. Flashes, Beeps, and Shakes with labels for each button.

--- app/src/main/java/com/example/angel/flashbeepshake/MainActivity.kt
+++ app/src/main/java/com/example/angel/flashbeepshake/MainActivity.kt
@@ -1,24 +1,64 @@
+// Got help from Sasha
 package com.example.angel.flashbeepshake
 
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toneGen:ToneGenerator
+    lateinit var vib:Vibrator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val camManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cam = camManager.cameraIdList[0]
+        toggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            camManager.setTorchMode(cam, isChecked)
         }
+
+        toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, 50)
+        toggleButton2.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if(isChecked)
+            {
+                toneGen.startTone(ToneGenerator.TONE_DTMF_3)	//play specific tone for 600ms
+            }
+            else
+            {
+                toneGen.stopTone()
+            }
+        }
+
+        vib = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        toggleButton3.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+            {
+                vib.vibrate(100000)
+            }
+            else
+            {
+                vib.cancel()
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
